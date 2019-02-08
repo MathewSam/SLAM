5545e1fc8f1a2ca586d03597a1977db42842b44b
MathewSam <m1sam@eng.ucsd.edu>
2019-02-07 23:07:58 -0800

    Added RGBD data

--- load_data.py
+++ load_data.py
@@ -1,9 +1,14 @@
 import numpy as np
 
+#class mapping:
+#  def __init__(self,):
+#    '''
+#    '''
+
 
 if __name__ == '__main__':
   dataset = 20
-  
+
   with np.load("Encoders%d.npz"%dataset) as data:
     encoder_counts = data["counts"] # 4 x n encoder counts
     encoder_stamps = data["time_stamps"] # encoder time stamps
@@ -15,14 +20,15 @@ if __name__ == '__main__':
     lidar_range_min = data["range_min"] # minimum range value [m]
     lidar_range_max = data["range_max"] # maximum range value [m]
     lidar_ranges = data["ranges"]       # range data [m] (Note: values < range_min or > range_max should be discarded)
-    lidar_stamsp = data["time_stamps"]  # acquisition times of the lidar scans
+    lidar_stamps = data["time_stamps"]  # acquisition times of the lidar scans
     
   with np.load("Imu%d.npz"%dataset) as data:
     imu_angular_velocity = data["angular_velocity"] # angular velocity in rad/sec
     imu_linear_acceleration = data["linear_acceleration"] # Accelerations in gs (gravity acceleration scaling)
     imu_stamps = data["time_stamps"]  # acquisition times of the imu measurements
-  
+
   with np.load("Kinect%d.npz"%dataset) as data:
     disp_stamps = data["disparity_time_stamps"] # acquisition times of the disparity images
     rgb_stamps = data["rgb_time_stamps"] # acquisition times of the rgb images
 
+print(disp_stamps)
